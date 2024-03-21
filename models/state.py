9ca16e8c9f58d6aa579a55a7e2409966f8923a3f
Yosrahabouria <MY_NAME@example.com>
2024-03-21 06:46:09 -0700

    airbnb

--- models/state.py
+++ models/state.py
@@ -1,8 +1,28 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.city import City
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """ class definition
+    Attributes:
+        name: input name
+    """
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade="all,delete", backref="state")
+
+    @property
+    def cities(self):
+        """  Def of cities"""
+        from models import storage
+        allcities = storage.all(City)
+        k = []
+        for i, j in allcities.items():
+            if j.state_id == self.id:
+                k.append(j)
+        return k
